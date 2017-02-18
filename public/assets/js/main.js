0ea9bd8381da1168af414a1678727257dd40544e
aries0d0f <aries0d0f@gmail.com>
2017-02-18 12:23:15 +0800

    add auto callback

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,7 +14,8 @@ var msgs = [];
 
 $(document).ready(function () {
     setInterval("time()", 1000);
-    setInterval("callbackFunction", 1000);
+    setInterval("update_room_data", 20000);
+    setInterval("update_yahoo", 10000);
     $("#shift").click(function () {
         $(this).toggleClass("active");
     });
@@ -35,6 +36,11 @@ $(document).ready(function () {
         }
         msgs.push(airMsg);
     });
+    update_room_data();
+    update_yahoo();
+});
+
+function update_room_data() {
     $.getJSON("/config", (data) => {
         var url = data.url;
         $.getJSON(url, (data) => {
@@ -44,51 +50,52 @@ $(document).ready(function () {
             document.getElementById("hum").innerText = "Hum : " + hum + "%";
         });
     });
-});
+}
 
+function update_yahoo() {
+    $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='taipei, tw') and u='c'&format=json", (callback) => {
+        var data = callback.query.results.channel;
+        var weather = data.item.condition.temp;
+        var wc = " " + data.item.condition.text;
+        var currentdate = new Date();
+        var sunrise = data.astronomy.sunrise.slice(0, -3).split(":");
+        var sunset = data.astronomy.sunset.slice(0, -3).split(":");
+        var sunriseTime = (parseInt(sunrise[0], 10)) * 60 + (parseInt(sunrise[1], 10));
+        var sunsetTime = (parseInt(sunset[0], 10) + 12) * 60 + (parseInt(sunset[1], 10));
+        var nowTime = currentdate.getHours() * 60 + currentdate.getMinutes();
 
-var callbackFunction = function (callback) {
-    var data = callback.query.results.channel;
-    var weather = data.item.condition.temp;
-    var wc = data.item.condition.text;
-    var currentdate = new Date();
-    var sunrise = data.astronomy.sunrise.slice(0, -3).split(":");
-    var sunset = data.astronomy.sunset.slice(0, -3).split(":");
-    var sunriseTime = (parseInt(sunrise[0], 10)) * 60 + (parseInt(sunrise[1], 10));
-    var sunsetTime = (parseInt(sunset[0], 10) + 12) * 60 + (parseInt(sunset[1], 10));
-    var nowTime = currentdate.getHours() * 60 + currentdate.getMinutes();
-
-    if (wc.indexOf('Cloudy') > 0) {
-        var imgLink = 'clouds';
-    }
-    if (wc.indexOf('Sunny') > 0) {
-        var imgLink = 'clear';
-        msgs.push("Remember to bring an umbrella with you!");
-    }
-    if (wc.indexOf('Rain') > 0) {
-        var imgLink = 'rain';
-        msgs.push("Remember to bring an umbrella with you!");
-    }
-    if (wc.indexOf('Shower') > 0) {
-        var imgLink = 'showers';
-        msgs.push("Remember to bring an umbrella with you!");
-    }
-    if ((nowTime > sunsetTime) | (nowTime < sunriseTime)) {
-        var time = 'night';
-    } else {
-        var time = 'day';
-    }
-    if (weather <= 25) {
-        msgs.push("Remember warm clothes!");
-    }
-    $(".weather>img").attr('src', '/assets/images/icon/weather-' + imgLink + '-' + time + '.png');
-    $("#next").css("background-image", "url(/assets/images/" + time + "-bg.jpg)");
-    var app = new Vue({
-        el: '#next',
-        data: {
-            weather: weather,
-            wc: wc,
-            msgs: msgs
+        if (wc.indexOf('Cloudy') > 0) {
+            var imgLink = 'clouds';
         }
+        if (wc.indexOf('Sunny') > 0) {
+            var imgLink = 'clear';
+            msgs.push("Remember to bring an umbrella with you!");
+        }
+        if (wc.indexOf('Rain') > 0) {
+            var imgLink = 'rain';
+            msgs.push("Remember to bring an umbrella with you!");
+        }
+        if (wc.indexOf('Shower') > 0) {
+            var imgLink = 'showers';
+            msgs.push("Remember to bring an umbrella with you!");
+        }
+        if ((nowTime > sunsetTime) | (nowTime < sunriseTime)) {
+            var time = 'night';
+        } else {
+            var time = 'day';
+        }
+        if (weather <= 25) {
+            msgs.push("Remember warm clothes!");
+        }
+        $(".weather>img").attr('src', '/assets/images/icon/weather-' + imgLink + '-' + time + '.png');
+        $("#next").css("background-image", "url(/assets/images/" + time + "-bg.jpg)");
+        var app = new Vue({
+            el: '#next',
+            data: {
+                weather: weather,
+                wc: wc,
+                msgs: msgs
+            }
+        })
     })
-};
+}
